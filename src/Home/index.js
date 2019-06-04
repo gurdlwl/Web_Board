4e93ccee5f3c9a74b4a43383c3662ecd91751d08
gurdlwl <gurdlwl@gmail.com>
2019-06-04 16:11:16 +0900

    [UPDATE]

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,26 +1,16 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
+import Board from '../Board';
+import './index.scss'
 
 @inject('stores')
 @observer
 class Home extends Component {
-    state = {
-        value: 1
-    };
 
     render() {
-        let t = this.props.stores.TimeStore;
-        let p = this.props.stores.PostStore;
-
         return (
             <div>
-                <div>{p.current_time && p.current_time}</div>
-                <div>{p.ms}</div>
-                <div><button onClick={p.getTime}>getTime</button></div>
-
-                <div>{t.current_time && t.current_time.toString()}</div>
-                <div>{t.ms}</div>
-                <div><button onClick={t.getTime}>getTime</button></div>
+                <Board />
             </div>
         );
     }
--- src/Stores/PostStore.js
+++ src/Stores/PostStore.js
@@ -1,5 +1,5 @@
 import {observable, action} from "mobx";
-
+import axios from 'axios';
 import TimeStore from './TimeStore';
 
 class PostStore{
@@ -19,6 +19,27 @@ class PostStore{
 
     @observable current_time = null;
     @action getTime = async () => this.current_time = await new Date().getTime();
+
+    @observable items = null;
+    @action fetchItems = async () => {
+        try{
+            let response = await axios({ // axios : server에서 data를 가지고 옴. 즉각적으로 반응이 오지않음 -> async 사용 필요
+                url: 'http://localhost:8080/api/getBoards',
+                method: 'get',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8'
+                },
+                timeout: 3000 // 3초 내에 응답이 없으면 에러를 띄움
+            });
+            console.log(response);
+
+            if(response.status === 200)
+                this.items = response.data;
+        } catch(err){
+            console.log(err.toLocaleString());
+        }
+
+    }
 }
 
 export default PostStore.getInstance();
